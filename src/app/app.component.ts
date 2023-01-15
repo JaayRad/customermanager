713c8a43ce0e5e7ae50ca5d74547689b68367cfa
jaayrad <zakubrad@gmail.com>
2023-01-15 04:11:46 +0100

    files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,63 @@
 import { Component } from '@angular/core';
+import { Customer, Address } from './model';
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+  <div class="container bg-light">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h1 [style.color] = "nameColor"  class="text-left m-4" [class.isActive] ="isActive" >{{ customer.name.toUpperCase() }}</h1>
+      </div>
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
+    <div class="panel-body text-center">
+      <div class="row">
+        <div class="col-sm-8">
+        <p>{{ customer.description }}</p>
+        <p>Wiek: {{ customer.age }}</p>
+        <p></p>
+        <br>
+        <button (click)="change()" type="button" class="btn btn-primary m-5">Przełącz podkreślenie</button>
+        <br>
+        <input #nameInput type="text" [value]="customer.name" (input)="customer.name = nameInput.value">
+        </div>
+
+        <div class="col-sm-4">
+        <img [src]="customer.photoUrl" >
+        </div>
+      </div>
+      
+    </div>
+  </div>
   `,
-  styles: []
+  styles: [
+    '.isActive {text-decoration: underline; }'
+  ]
 })
+
 export class AppComponent {
-  title = 'customermanager';
+  customer: Customer = {
+    name: "Jan Kowalski",
+    photoUrl: "assets/images/eustachy.jpg",
+    description: "Farmer",
+    age: 70,
+  }
+  address: Address = {
+    street: "Nowhere in Desert",
+    houseNumber: 1,
+    city: "Phoenix",
+
+  }
+
+  nameColor: string = 'blue';
+  isActive: boolean = true;
+
+  constructor(){
+
+  }
+
+  change(){
+    this.isActive = !this.isActive;
+  }
+
 }
---
+++ src/app/model.ts
@@ -0,0 +1,13 @@
+export interface Customer{
+    name: string;
+    photoUrl: string;
+    description: string;
+    age: number;
+}
+
+export interface Address{
+    street: string;
+    houseNumber: number;
+    city: string;
+
+}
